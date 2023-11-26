38d3fe26c2f9035a9c2f6f83f4298782f19cb438
Miłosz Pigłas <milosz@archeocs.com>
2023-11-26 19:58:43 +0100

    feat: count executions and save in Azure Storage table

--- function_app.py
+++ function_app.py
@@ -1,6 +1,11 @@
-from datetime import datetime
 import logging
+import os
+
 import azure.functions as func
+
+from datetime import datetime
+
+from azure.data.tables import TableServiceClient, UpdateMode
 
 app = func.FunctionApp()
 
@@ -11,7 +16,38 @@ app = func.FunctionApp()
               arg_name="timer",
               run_on_startup=True)
 def handle(timer):
-    now = datetime.datetime.now().isoformat()
+    now = datetime.now().isoformat()
     if timer.past_due:
         logging.info('The timer is past due!')
     logging.info('Python timer trigger function ran at %s', now)
+
+    connection_str = os.environ.get("connection_string")
+    table_name = os.environ.get("table_name")
+
+    logging.info("Saving data to table {}".format(table_name))
+
+    storage_cli = TableServiceClient.from_connection_string(connection_str)
+    table_cli = storage_cli.get_table_client(table_name)
+    pk = os.environ.get("function_name")
+
+    logging.info("Read function {}".format(pk))
+
+    entity = {
+        "PartitionKey": pk,
+        "RowKey": "execution",
+        "Count": 0
+    }
+    try:
+        saved = table_cli.get_entity(pk, "execution")
+        logging.info("Execution entity {}".format(entity))
+        entity["Count"] = saved.get("Count", 0) + 1
+        
+    except Exception as e:
+        logging.error("Reading entity error {}".format(e))
+
+    logging.info("Upsert entity {}".format(entity))
+    table_cli.upsert_entity(entity)
+
+    
+    
+    
